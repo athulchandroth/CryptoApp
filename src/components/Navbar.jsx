18d04f8badb01aa554f16a9d501f55e919ad073e
athulavenger018 <athul.c.tv@gmail.com>
2023-04-05 15:16:55 +0530

    minor fixes

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,29 @@
-import React,{useState} from 'react';
+import React,{useState,useEffect} from 'react';
 import { Button,Menu,Typography,Avatar } from 'antd';
 import {Link} from 'react-router-dom';
 import{HomeOutlined,MoneyCollectOutlined,BulbOutlined,FundOutlined,MenuOutlined, CloseOutlined } from '@ant-design/icons';
 import icon from '../images/cryptocurrency.png';
 const Navbar = () => {
     const [activeMenu,setActiveMenu] = useState(true);
+    const [screenSize, setScreenSize] = useState(undefined);
+
+    useEffect(() => {
+      const handleResize = () => setScreenSize(window.innerWidth);
+  
+      window.addEventListener('resize', handleResize);
+  
+      handleResize();
+  
+      return () => window.removeEventListener('resize', handleResize);
+    }, []);
+  
+    useEffect(() => {
+      if (screenSize <= 800) {
+        setActiveMenu(false);
+      } else {
+        setActiveMenu(true);
+      }
+    }, [screenSize]);
 
   return (<div className='nav-container'>
       <div className='logo-container'>
@@ -21,25 +40,28 @@ const Navbar = () => {
                 }
           </Button>
       </div>
-      {
-            activeMenu && ( 
-      <Menu theme='dark'>
+      
+{
+    activeMenu && (
+      <Menu theme='dark' >
           <Menu.Item icon ={<HomeOutlined/>}>
               <Link to="/">Home</Link>
           </Menu.Item>
           <Menu.Item icon ={<FundOutlined/>}>
               <Link to="/cryptocurrencies">Cryptocurrencies</Link>
           </Menu.Item>
-          <Menu.Item icon ={<MoneyCollectOutlined/>}>
+          {/* <Menu.Item icon ={<MoneyCollectOutlined/>}>
               <Link to="/exchanges">Exchanges</Link>
-          </Menu.Item>
+          </Menu.Item> */}
           <Menu.Item icon ={<BulbOutlined/>}>
               <Link to="/news">News</Link>
           </Menu.Item>
           
       </Menu>
-            )
-      }
+    )
+}
+            
+      
   </div>);
 };
 
